e7cb1e9f65fca4d9de1ccd0b207806fab2dfb316
MasksofReality <94428371+MasksofReality@users.noreply.github.com>
2026-07-15 00:01:05 -0500

    7.15.1201
    clean up small tweaks to color

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
 
   eleventyConfig.addFilter("splatIcon", function (splat) {
     const icons = {
